2887c98018a199da8496129f3060905774a8c32c
Christina Bui <68668936+cttub@users.noreply.github.com>
2022-07-29 20:12:57 -0500

    Location file

---
+++ src/Components/Location.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react';
+import { render } from '@testing-library/react';
+import '../Styles/Location.css';
+
+
+
+class Location extends Component{
+    render(){
+        return(
+            
+            <div className = "MainLocation" id = 'location'>
+            
+          
+            
+            </div>
+
+     
+        )
+    }
+}
+
+
+export {Home};
